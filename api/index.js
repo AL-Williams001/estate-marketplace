75a123ea41708de19102a527223735097c95ddf0
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-05-22 18:25:24 +0800

    create error handler and middleware

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/userRoute.js";
 import authRouter from "./routes/authRoute.js";
 dotenv.config();
 
+// Mongo DB connection
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
@@ -22,5 +23,17 @@ app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
 
+// routes
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+//Middleware
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong!";
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
+});
